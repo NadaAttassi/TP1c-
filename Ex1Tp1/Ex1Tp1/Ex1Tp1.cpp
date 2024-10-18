da9316a44a66a59bcca07268b51c39c54186aeb5
NadaAttassi <167885570+NadaAttassi@users.noreply.github.com>
2024-10-18 08:50:31 +0100

    Update Ex1Tp1.cpp

--- Ex1Tp1/Ex1Tp1/Ex1Tp1.cpp
+++ Ex1Tp1/Ex1Tp1/Ex1Tp1.cpp
@@ -3,18 +3,42 @@
 
 #include <iostream>
 
+#include "Complexe.h"
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Création de deux nombres complexes
+    Complexe z1(3, 4);
+    Complexe z2(1, 2);
+
+    // Affichage des nombres complexes
+    z1.afficher();
+    z2.afficher();
+
+    // Addition des deux nombres complexes
+    Complexe somme = z1 + z2;
+    somme.afficher();
+
+    // Soustraction
+    Complexe difference = z1 - z2;
+    difference.afficher();
+
+    // Multiplication
+    Complexe produit = z1 * z2;
+    produit.afficher();
+
+    // Division
+    
+        Complexe division = z1 / z2;
+        division.afficher();
+
+
+    // Module
+    std::cout << "Module de z1: " << z1.module() << "\n";
+
+    // Conjugué
+    Complexe conj = z1.conjugué();
+    conj.afficher();
+
+    return 0;
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
